84e60b74adfa6efc55cc651fc57cf9ba50edf12d
soracat <125882337+s0racat@users.noreply.github.com>
2024-10-03 21:54:23 +0900

    Add home-manager to flake inputs

--- flake.nix
+++ flake.nix
@@ -1,18 +1,33 @@
 {
   description = "Minimal NixOS installation media";
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-  outputs = { self, nixpkgs }: {
-    nixosConfigurations = {
-      exampleIso = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          ({ pkgs, modulesPath, ... }: {
-            imports = [ (modulesPath + "/installer/cd-dvd/installation-cd-base.nix")
-	    (./configuration.nix)];
-          })
-        ];
-      };
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
   };
+  outputs =
+    { self, nixpkgs }@inputs:
+    {
+      nixosConfigurations = {
+        exampleIso = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          specialArgs = {
+            inherit inputs;
+          };
+          modules = [
+            (
+              { pkgs, modulesPath, ... }:
+              {
+                imports = [
+                  (modulesPath + "/installer/cd-dvd/installation-cd-base.nix")
+                  (./configuration.nix)
+                ];
+              }
+            )
+          ];
+        };
+      };
+    };
 }
-
