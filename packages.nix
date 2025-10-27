e610ce6747359f6a228cbbd40a7bbbc10547ae77
Takumi <125882337+s0racat@users.noreply.github.com>
2025-10-27 12:31:40 +0900

    Comment out keepassxc in systemPackages

--- packages.nix
+++ packages.nix
@@ -2,7 +2,7 @@
 {
   environment.systemPackages = with pkgs; [
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-    keepassxc
+    #keepassxc
     p7zip
     eza
   ];
