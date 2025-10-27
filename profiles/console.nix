8f66aaf8c4f2d1cf9e623a5462437b9492004c41
Takumi <125882337+s0racat@users.noreply.github.com>
2025-10-27 12:34:00 +0900

    Remove unused XFCE and service configurations

---
+++ profiles/console.nix
@@ -0,0 +1,16 @@
+{ self, pkgs, ... }:
+{
+  imports = [
+    "${self}/boot.nix"
+    "${self}/misc.nix"
+    "${self}/nix.nix"
+    "${self}/packages.nix"
+    "${self}/programs/htop.nix"
+    "${self}/programs/bat.nix"
+    "${self}/programs/zsh.nix"
+    "${self}/systemd.nix"
+    "${self}/services/tlp.nix"
+    "${self}/user-nixos.nix"
+  ];
+ 
+}
