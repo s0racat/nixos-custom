e7516ede88d67d5e6892ecd9b4a686f7174b9318
Takumi <125882337+s0racat@users.noreply.github.com>
2025-12-05 13:43:38 +0900

    Refactor nixosSystem configuration in util.nix
    Remove stateVersion from specialArgs in nixosSystem.

--- lib/util.nix
+++ lib/util.nix
@@ -15,16 +15,13 @@ in
       name,
       system ? "x86_64-linux",
       specialArgs ? { inherit inputs self; },
-      stateVersion ? "24.11",
       extraModules ? [ ],
     }:
     {
       inherit name;
       value = nixpkgs.lib.nixosSystem {
         pkgs = nixpkgsFor.${system};
-        specialArgs = specialArgs // {
-          inherit stateVersion;
-        };
+        inherit specialArgs;
         modules = [
           inputs.home-manager.nixosModules.home-manager
           "${self}/profiles/${name}.nix"
